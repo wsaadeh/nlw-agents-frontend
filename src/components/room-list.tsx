a4a34295347254a1ebe43dc3373d3cad93c018a3
Wilson Saadeh <wilson_saadeh@hotmail.com>
2025-07-11 23:43:05 -0300

    feat: room-list create-room-form question-form question-item + components

---
+++ src/components/room-list.tsx
@@ -0,0 +1,58 @@
+import { ArrowRight } from 'lucide-react';
+import { Link } from 'react-router-dom';
+import { Badge } from '@/components/ui/badge';
+import { useRooms } from '@/http/use-rooms';
+import { dayjs } from '@/lib/dayjs';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from './ui/card';
+
+export function RoomList() {
+  const { data, isLoading } = useRooms();
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Salas Recentes</CardTitle>
+        <CardDescription>
+          Acesso rápido para as salas criadas recetemente
+        </CardDescription>
+      </CardHeader>
+      <CardContent className="flex flex-col gap-3">
+        {isLoading && (
+          <p className="text-muted-foreground text-sm">Carregando salas...</p>
+        )}
+        {data?.map((room) => {
+          return (
+            <Link
+              className="flex items-center justify-between rounded-lg border p-3 hover:bg-accent/50"
+              key={room.id}
+              to={`/room/${room.id}`}
+            >
+              <div className="flex flex-1 flex-col gap-1">
+                <h3 className="font-medium">{room.name}</h3>
+
+                <div className="flex items-center gap-2">
+                  <Badge className="text-xs" variant="secondary">
+                    {dayjs(room.createdAt).toNow()}
+                  </Badge>
+                  <Badge className="text-xs" variant="secondary">
+                    {room.questionsCount} pergunta(s)
+                  </Badge>
+                </div>
+              </div>
+
+              <span className="flex items-center gap-1 text-sm">
+                Entrar
+                <ArrowRight className="size-3" />
+              </span>
+            </Link>
+          );
+        })}
+      </CardContent>
+    </Card>
+  );
+}
